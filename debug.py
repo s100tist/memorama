e31fd6827cee3d4ee65a03d09083875fb84c0d0a
snifex <58663377+snifex@users.noreply.github.com>
2022-11-30 13:57:04 -0600

    Teclas chingon

--- debug.py
+++ debug.py
@@ -1,11 +1,33 @@
 import keyboard as kb
+from time import sleep
 from Tablero import Tablero
 
 
 tablero = Tablero()
 tablero.crearTablero(6)
 
+def detectarTecla(): 
+    tecla_str : str = ""
+    while(True):
+        if kb.is_pressed("up"):   
+            tecla_str = "up"
+            break
 
-while(1):
-    kb.wait("up")
-    print("o")
+        elif kb.is_pressed("down"):
+            tecla_str = "down"
+            break
+
+        elif kb.is_pressed("right"):
+            tecla_str = "right"
+            break
+
+        elif kb.is_pressed("left"):
+            tecla_str = "left"
+            break
+
+    return tecla_str
+
+
+while(True):
+    sleep(0.1)
+    print(detectarTecla())
