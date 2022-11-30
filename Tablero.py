c0102e904b3ca139d2ebf2dd83c67f515bb70e23
Rugal <scaryskriller@gmail.com>
2022-11-30 01:40:09 -0600

    avance chingon >:(

--- Tablero.py
+++ Tablero.py
@@ -1,4 +1,5 @@
 import randomizacion
+from Casilla import Casilla
 
 class Tablero:
     puntuacion:             int
@@ -8,6 +9,8 @@ class Tablero:
     #Constructor
     def __init__(self): 
         self.puntuacion = 0
+        self.casilla_seleccionada = {'x':0,'y':0}
+        self.casillas=[list(range(6)) for _ in range(6)]
 
     #Getters and setters
     # def __getattribute__(self, __name: str) -> Any:
@@ -16,21 +19,34 @@ class Tablero:
     def get_puntuacion(self) -> int:
         return self.puntuacion
     
+    def get_casillas(self)-> list:
+        return self.casillas
+
     # Metodos
 
 
-    def crearTablero(self, tamanio_tablero: int):
-        #creamos una matriz de 'tamaño' x 'tamaño'
-        tab2 = self.__init__
-        tab2 = [list(range(tamanio_tablero)) for _ in range(tamanio_tablero)]
-        emojis = randomizacion.randomizarEmojis()
+    def crearTablero(self, tamanio_tablero: int)->None:
         conta = 0
+        emojis = randomizacion.randomizarEmojis()
         for i in range(tamanio_tablero):
             for j in range(tamanio_tablero):
-                tab2 [i][j] = emojis[conta]
-                conta = conta + 1  
+                casilla = Casilla()
+                casilla.set_simbolo(emojis[conta])
+                self.casillas[i][j] = casilla
+                conta += 1
 
-        return tab2
+    def imprimirCasillas(self)->None:
+        for i in range(len(self.casillas)):
+            for j in range(len(self.casillas[0])):
+                if self.casillas[i][j].get_es_visible() == True:
+                    print (self.casillas[i][j].get_simbolo(),end="")
+                else:
+                    print('🛑',end="")
+            print("")
+
+    def cambiarEstadoCasilla(self,coordenadas: dict)->None:{
+        self.casillas[coordenadas['x']][coordenadas['y']].set_es_visible()
+    }
 
         
 
